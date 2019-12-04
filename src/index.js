81df859848347dfadca168d48e05f6f006445bf4
LyraLe <951187248@qq.com>
2019-12-04 19:10:29 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 /*
  * @Author: LyraLee
  * @Date: 2019-11-30 07:51:39
- * @LastEditTime: 2019-12-04 18:39:29
+ * @LastEditTime: 2019-12-04 19:10:21
  * @Description: file content
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ContentLoader from 'react-content-loader';
 
-//
+
 const style = {
   width: '100%',
   height: 300,
